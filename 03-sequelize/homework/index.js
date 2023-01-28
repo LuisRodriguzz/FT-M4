28e9203cf7f6345393ece69d5672cf2d3b28e2f0
unknown <luisrodriguzzr@gmail.com>
2023-01-28 16:56:01 -0500

    Finalizado

---
+++ 03-sequelize/homework/index.js
@@ -0,0 +1,10 @@
+const app = require('./server');
+const { db } = require('./db');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  //db.sync({ force: true });//drop a tablas
+
+  db.sync() // se mantienen los datos
+});
